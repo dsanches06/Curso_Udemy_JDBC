85249838ef77ae3c1300dcc9b8cf1f979e9753a0
Danilson Sanches <dsanches06@outlook.com>
2020-03-10 17:51:58 +0000

    insert data with return of id

--- src/application/Insert.java
+++ src/application/Insert.java
@@ -2,9 +2,12 @@ package application;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+
 import db.DB;
 
 public class Insert {
@@ -22,25 +25,39 @@ public class Insert {
 			conn = DB.getConnection();
 			// criar o preparedstatement apartir da conexao e recebe a squery
 			st = conn.prepareStatement(
-					"INSERT INTO seller (Name, Email, Birthdate, BaseSalary, DepartmentId) VALUES (?, ?, ?, ?, ?)");
+					"INSERT INTO seller (Name, Email, Birthdate, BaseSalary, DepartmentId) VALUES (?, ?, ?, ?, ?)",
+					//usar para obter o id da tabela
+					Statement.RETURN_GENERATED_KEYS);
 
 			// inserir os dados
-			st.setString(1, "Danilson Sanches");
-			st.setString(2, "dsanches06@gmail.com");
+			st.setString(1, "Ivan Santos");
+			st.setString(2, "ivan@hotmail.com");
 			// inserir data para database atraves do java.sql.Date
-			st.setDate(3, new java.sql.Date(sdf.parse("06/07/1982").getTime()));
-			st.setDouble(4, 1200.00);
-			st.setInt(5, 1);
+			st.setDate(3, new java.sql.Date(sdf.parse("08/02/1990").getTime()));
+			st.setDouble(4, 1800.00);
+			st.setInt(5, 4);
 
 			// executar a squery para alterar os dados da tabela, inserção ou edição
 			int rows = st.executeUpdate();
-			System.out.println("ROWS: " + rows);
+			
+			if (rows > 0) {
+				//obter o resultado da squery em resultset
+				ResultSet rs = st.getGeneratedKeys();
+				//percorrer o resultset
+				while(rs.next()) {
+					//obter o id, colocando valor 1 para obter o valor da primeira coluna
+					int id = rs.getInt(1);
+					System.out.println("Id: "+id);
+				}
+				//fechar result set
+				DB.closeResultSet(rs);
+			}else {
+				System.out.println("No rows affected!");
+			}
 
-		}
-		catch (SQLException | ParseException e) {
+		} catch (SQLException | ParseException e) {
 			System.out.println("Error: " + e.getMessage());
-		} 
-		finally {
+		} finally {
 			DB.closeStatement(st);
 			DB.closeConnection();
 		}
