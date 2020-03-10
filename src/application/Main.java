367828d607093bd3cb71b924791e189933694b24
Danilson Sanches <dsanches06@outlook.com>
2020-03-10 17:06:57 +0000

    retrieve data from database

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,7 +1,9 @@
 package application;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 import db.DB;
 
@@ -9,10 +11,30 @@ public class Main {
 
 	public static void main(String[] args) throws SQLException {
 
-		//criar uma nova conexao
-		Connection conn = DB.getConnection();
-		//fechar a conexao
-		DB.closeConnection();
+		// para criar conexao
+		Connection conn = null;
+		// para criar o statement para executar squery
+		Statement stat = null;
+		// para obter o resultado da squery em forma de tabela
+		ResultSet rs = null;
 
+		try {
+			// criar conexao
+			conn = DB.getConnection();
+			// criar um statement apartir da conexao
+			stat = conn.createStatement();
+			// resultset será igual ao statement execute squery
+			rs = stat.executeQuery("select * from department");
+			// usando metodo next() do resultset para percorrer o resultado
+			while (rs.next()) {
+				System.out.println(rs.getInt("Id") + ", " + rs.getString("Name"));
+			}
+		} catch (SQLException e) {
+			System.out.println("Error: " + e.getMessage());
+		} finally {
+			DB.closeStatement(stat);
+			DB.closeResultSet(rs);
+			DB.closeConnection();
+		}
 	}
 }
